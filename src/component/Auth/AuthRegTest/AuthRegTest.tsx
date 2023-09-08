1a33ac53f1d5c8f4a61f584b0543eb8317c1dd98
jjh0428 <92361061+jjh0428@users.noreply.github.com>
2023-09-08 17:01:25 +0900

    save

--- src/component/Auth/AuthRegTest/AuthRegTest.tsx
+++ src/component/Auth/AuthRegTest/AuthRegTest.tsx
@@ -1,54 +1,85 @@
 import * as React from 'react';
-import {UserStatus} from "../../../@types";
 import styled from "styled-components";
-import {Controller, useForm} from "react-hook-form";
+import {FieldValues, FormProvider, useForm} from "react-hook-form";
+import FormName from "../../Input/FormName";
 
-type AuthRegType = {
-    name: string;
-    email: string;
-    phone: string;
-    password: string;
-    status: UserStatus;
-}
-
-const Form = styled.form`
-  > div:not(:first-child) {
-    margin-top: 30px;
-  }
-  
-  > div:not(:last-child) {
-    position: relative;
-  }
-  
-  > div > label {
-  display: inline-block;
-  padding-bottom: 8px;
-}
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 130px;
 `;
 
+const Form = styled.form`
+  width: 300px;
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 30px;
+`;
+
+interface IFormData extends FieldValues {
+    name: string;
+    email: string;
+}
+
 const AuthRegTest: React.FC = () => {
-    const { handleSubmit, control } = useForm<AuthRegType>({
-        // resolver: yupResolver(signUpValidation),
-        mode: 'onBlur',
+    const methods = useForm<IFormData>({
+        defaultValues: {
+            name: '',
+            email: '',
+        },
+        mode: 'onChange',
     });
 
-    const onSubmin = handleSubmit((data: AuthRegType) => {
+    const {
+        handleSubmit
+    } = methods;
+
+    const onSubmit = (data:IFormData) => {
         console.log(data);
-    });
+    };
 
   return (
-    <Form>
-        <div>
-            <label htmlFor='name'>아이디</label>
-            <Controller
-                type='text'
-                name='name'
-                control={control}
-                placeholder='아이디를 입력해주세요'
-                defaultValue=''
-            />
-        </div>
-    </Form>
+      <FormProvider {...methods}>
+          <Container>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+              <FormName
+                  label='이름'
+                  name='name'
+                  rules={{
+                      required: '이름을 입력해주세요',
+                      pattern: {
+                          value: /^[^ ]*[^ ]$/,
+                          message: '앞뒤 공백을 지워주세요',
+                      },
+                      minLength: {
+                          value: 2,
+                          message: '2글자 이상 입력해주세요'
+                      },
+                  }}
+                  type='text'
+                  placeholder='이름을 입력해주세요'
+                  helperText='2글자 이상'
+               />
+              <FormName
+                  label='이메일'
+                  name='email'
+                  rules={{
+                      required: '이메일을 입력해주세요',
+                      pattern: {
+                          value: /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+                          message: '이메일을 형식에 맞게 입력해주세요,'
+                      }
+                  }}
+                  type='text'
+                  placeholder='이메일을 입력해주세요'
+                  helperText='ex) text00@email.com'
+              />
+              <Button type='submit'>가입하기</Button>
+          </Form>
+          </Container>
+      </FormProvider>
   );
 };
 export default AuthRegTest;
--- src/component/Input/FormName/FormName.tsx
+++ src/component/Input/FormName/FormName.tsx
@@ -1,11 +1,76 @@
-import * as React from 'react';
+import { FieldPath, FieldValues, useController, UseControllerProps } from 'react-hook-form';
+import styled from 'styled-components';
 
-const FormName = () => {
+interface InputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> extends UseControllerProps<TFieldValues, TName> {
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  helperText?: string;
+}
+
+const Wrapper = styled.div<{ error?: boolean }>`
+  display: flex;
+  margin-bottom: 14px;
+  flex-direction: column;
+  
+  input {
+    border: ${(props) => (props.error ? `1px solid #d50000` : `1px solid black`)};
+  }
+  
+  span {
+    font-size: 14px;
+    color: ${(props) => (props.error ? `#d50000` : `black`)};
+  }
+  
+  .helperText {
+    color: #0000d5;
+    font-size: 15px;
+    display: ${(props) => (props.error ? 'none' : 'block')};
+  }
+`;
+
+const Input = styled.input`
+  display: flex;
+  height: 40px;
+  width: 100%;
+  outline: none;
+  padding-left: 5px;
+  margin: 3px 0;
+`;
+
+const Label = styled.label`
+  font-size: 17px;
+
+  span {
+    color: #d50000;
+  }
+`;
+
+function FormName<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(props: InputProps<TFieldValues, TName>) {
+  const name = props.name;
+  const label = props.label;
+  const helperText = props.helperText;
+  const { field, fieldState } = useController({
+    name,
+    rules: props.rules,
+  });
 
   return (
     <>
-
+      <Label>{label} <span>*</span></Label>
+      <Wrapper error={fieldState.error && true}>
+        <Input {...field} type={props.type} placeholder={props.placeholder} />
+        <span>{fieldState.error && fieldState.error.message}</span>
+        <p className='helperText'>{helperText}</p>
+      </Wrapper>
     </>
   );
-};
+}
+
 export default FormName;
